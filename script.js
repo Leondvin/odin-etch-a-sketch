e0d8ba7bbc8af53a238491e2ccd380a60cac9595
Leondvin <krmozncr@gmail.com>
2026-08-04 19:25:22 +0300

    Refactor box creation logic into addBoxes()
    After refactoring the box creation logic into a reusable function, the
    addBoxes() function is called using currSize as its argument on page
    load.

--- script.js
+++ script.js
@@ -2,10 +2,14 @@ const boxContainer = document.querySelector("#box-container");
 const resizeBtn = document.querySelector("#resize-btn");
 let currSize = 16;
 
-for (let i = 0; i < 16**2; i++) {
-    const box = document.createElement("div");
-    box.classList.add("box");
-    boxContainer.appendChild(box);
-}
+const addBoxes = boxPerSide => {
+    for (let i = 0; i < boxPerSide**2; i++) {
+        const box = document.createElement("div");
+        box.classList.add("box");
+        boxContainer.appendChild(box);
+    }
+};
+addBoxes(currSize);
+
 const getBoxes = () => document.querySelectorAll(".box");
 getBoxes().forEach(box => box.addEventListener("mouseenter", e => e.target.classList.add("filled")));
