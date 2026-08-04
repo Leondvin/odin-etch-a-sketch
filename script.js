6c8069439b4d464a090d61e1fe50d5a04b01b476
Leondvin <krmozncr@gmail.com>
2026-08-04 16:22:42 +0300

    Create and append boxes on page load

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 const boxContainer = document.querySelector("#box-container");
+
+for (let i = 0; i < 16**2; i++) {
+    const box = document.createElement("div");
+    box.classList.add("box");
+    boxContainer.appendChild(box);
+}
